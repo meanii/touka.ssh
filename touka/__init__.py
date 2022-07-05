1ae21745962689751d4642b7be03020ebbc1c167
anii <anilchauhan@mogiio.com>
2022-07-05 16:43:27 +0530

    bug: fix purge bug

--- touka/__init__.py
+++ touka/__init__.py
@@ -6,6 +6,6 @@ __app_name__ = "touka"
 __about__ = "Awesome ssh manager, especially made for anii ☂️"
 __source__ = "https://github.com/meanii/touka.ssh/"
 __author__ = "Anil Chauhan <https://github.com/meanii>"
-__version__ = "2.2.7"
+__version__ = "2.2.8"
 
 DEFAULT_ROOT_DIR_PATH = Path(Path.home().joinpath(".meanii/touka.ssh/"))
